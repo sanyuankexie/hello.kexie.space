26cfa67d6b5c2d41a92c813598888ea97c03f19b
July Well <me@Therainisme.com>
2021-06-08 20:39:17 +0800

    fix CI

--- src/component/BallRoom/BallRoom.tsx
+++ src/component/BallRoom/BallRoom.tsx
@@ -216,7 +216,7 @@ class Client {
         }
 
 
-        this.ws = new WebSocket("wss://10.33.39.225:5203/connect")
+        this.ws = new WebSocket("wss://kexie.therainisme.com:5203/connect")
         this.ws.onerror = (e) => {
             console.error('ws error', e);
         };
