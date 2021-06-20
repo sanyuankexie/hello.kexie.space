f575147b529850e148eb3524cde60dbaa55834fb
July Well <me@Therainisme.com>
2021-06-20 13:22:20 +0800

    MusicPlayer -> handler

--- src/pages/MusicPlayer/index.tsx
+++ src/pages/MusicPlayer/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     SyncOutlined,
     StepBackwardFilled,
@@ -9,25 +9,68 @@ import {
     SoundFilled,
 } from '@ant-design/icons';
 import style from "./index.module.css";
+import Remixicon from '../../component/Remixicon/index';
 
 function MusicPlayer() {
+    const [isPlay, setIsPlay] = useState<boolean>(false);
+
+    const [musics, setMusics] = useState(["星辰大海", "Lost Game", "Flurite Eye’s Song"]);
+    const [select, setSelect] = useState("Flurite Eye’s Song");
+    function handleOnMusic(music: string) {
+        if (select === music) return;
+        setSelect(music);
+    }
+
+    const [pass, setPass] = useState(20);
+    function handleOnProgress(e: React.MouseEvent) {
+        setPass((e.clientX / window.innerWidth) * 100);
+    }
+
+    function handleOnMode() {
+
+    }
+
+    function handleOnPrevious() {
+
+    }
+
+    function handleOnPlay() {
+        setIsPlay(!isPlay);
+    }
+
+    function handleOnNext() {
+
+    }
+
+    function handleOnReplay() {
+
+    }
+
     return (
         <section className={style.container}>
             <div className={style.sidebar}>
                 <div className={`${style.item} ${style.user}`}>Therainisme</div>
-                <div className={`${style.item} ${style.active}`}>星辰大海</div>
-                <div className={style.item}>Lost Game</div>
+
+                {musics.map(music => {
+                    return (
+                        <div
+                            className={`${style.item} ${select === music ? style.active : ''}`}
+                            onClick={e => handleOnMusic(music)}>
+                            {music}
+                        </div>
+                    );
+                })}
+
             </div>
             <div className={style.main}>
                 <div className={style.background}></div>
                 <div className={style.lyric}>
                 </div>
 
-
                 <div className={style.panel}>
-                    <div className={style.progress}>
-                        <div className={style.pass}></div>
-                        <div className={style.circle}></div>
+                    <div className={style.progress} onClick={e => handleOnProgress(e)}>
+                        <div className={style.pass} style={{ width: `${pass}%` }}></div>
+                        <div className={style.circle} style={{ left: `${pass}%` }}></div>
                     </div>
 
                     <div className={style.info}>
@@ -39,25 +82,27 @@ function MusicPlayer() {
                     </div>
 
                     <div className={style.control}>
-                        <div className={style.mode}>
+                        <div className={style.item} onClick={e => handleOnMode()}>
                             <SyncOutlined />
                         </div>
-                        <div className={style.previous}>
+                        <div className={style.item} onClick={e => handleOnPrevious()}>
                             <StepBackwardFilled />
                         </div>
-                        <div className={style.play} >
-                            <PlayCircleFilled />
+                        <div className={style.item} onClick={e => handleOnPlay()}>
+                            {isPlay ? <PlayCircleFilled /> : <PauseCircleFilled />}
                         </div>
-                        <div className={style.next}>
+                        <div className={style.item} onClick={e => handleOnNext()}>
                             <StepForwardFilled />
                         </div>
-                        <div className={style.replay}>
+                        <div className={style.item} onClick={e => handleOnReplay()}>
                             <RedoOutlined />
                         </div>
                     </div>
 
                     <div className={style.volume}>
-                        <SoundFilled />
+                        <span className={style.item}>
+                            <Remixicon.VolumeUpFill className={style.item} />
+                        </span>
                     </div>
                 </div>
             </div>
