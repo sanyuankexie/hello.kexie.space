b766fc276f8ef5e6cc54e80ee59592799c01483f
July Well <me@Therainisme.com>
2021-07-04 21:36:48 +0800

    configurate theme color

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import './theme.less';
 
 import React from 'react';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true; @import (reference) "${resolve('src/theme.less')}";`,
+        },
+        javascriptEnabled: true,
+      }
+    }
+  },
 })
