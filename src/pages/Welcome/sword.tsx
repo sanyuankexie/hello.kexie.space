2eba47cf80fb261731a307870d7bf39c201e08ee
MirrorChilde <2940297105@qq.com>
2022-08-18 10:55:31 +0800

    增加了游戏 报名表

--- src/pages/Welcome/sword.tsx
+++ src/pages/Welcome/sword.tsx
@@ -10,14 +10,16 @@ function PromotionalVideo() {
         <Section
             title="Sword"
             description="科协游戏开发方向李一奔的作品"
+            url="全屏体验"
             bannerStyle={{ width: "80%", height: "80vh" }}>
-                <a href="http://game.kexie.space/">
-                     <img height="100%"
-                width="100%" 
-                src="../src/assets/images/sword.jpg" 
-                ref={addScrollAnimationRefs}   
-                style={{ boxShadow: "0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d" }} />
-                </a>
+                <iframe src="http://game.kexie.space/"
+                height="100%"
+                width="100%"
+                frameBorder="0"
+                scrolling="no"
+                style={{ boxShadow: "0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d" }}
+                ref={addScrollAnimationRefs}
+            ></iframe>
            
         </Section>
     );
