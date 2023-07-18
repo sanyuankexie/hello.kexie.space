72f77948ae8368f7a41c9517059c13033aa03efc
mosquito <mosquito@email.cn>
2023-07-18 15:41:14 +0800

    fix: mikutap

--- src/pages/Welcome/Mikutap.tsx
+++ src/pages/Welcome/Mikutap.tsx
@@ -3,23 +3,28 @@ import Section from "../../component/Section";
 import { useScrollHandler } from "../../hooks";
 
 function PromotionalVideo() {
-    const [ScrollAnimationRefs, addScrollAnimationRefs] = useScrollHandler();
+  const [ScrollAnimationRefs, addScrollAnimationRefs] = useScrollHandler();
 
-    return (
-        <Section
-            title="Mikutap"
-            description="一个有趣的音乐互动网站"
-            bannerStyle={{ width: "80%", height: "80vh" }}>
-            <iframe src="https://mikutap.therainisme.com"
-                height="100%"
-                width="100%"
-                frameBorder="0"
-                scrolling="0"
-                style={{ boxShadow: "0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d" }}
-                ref={addScrollAnimationRefs}
-            ></iframe>
-        </Section>
-    );
+  return (
+    <Section
+      title="Mikutap"
+      description="一个有趣的音乐互动网站"
+      bannerStyle={{ width: "80%", height: "80vh" }}
+    >
+      <iframe
+        src="https://miku.msqt.fun"
+        height="100%"
+        width="100%"
+        frameBorder="0"
+        scrolling="0"
+        style={{
+          boxShadow:
+            "0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d",
+        }}
+        ref={addScrollAnimationRefs}
+      ></iframe>
+    </Section>
+  );
 }
 
 export default PromotionalVideo;
