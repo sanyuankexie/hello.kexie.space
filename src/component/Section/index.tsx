4e97a21e159b875dc24372be923d9f17987b364c
wilinz <weilizan71@gmail.com>
2024-08-23 13:22:58 +0800

    适配移动端

--- src/component/Section/index.tsx
+++ src/component/Section/index.tsx
@@ -1,48 +1,54 @@
-import React, { CSSProperties } from "react";
-import { Typography } from "antd";
+import React, {CSSProperties} from "react";
+import {Typography} from "antd";
 import section from "./index.module.scss";
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 export interface IProps {
-  title: string;
-  description?: string;
-  url_title?: string;
-  url?: string;
-  children: JSX.Element | string;
-  bannerStyle?: CSSProperties;
-  bannerClassName?: string;
+    title: string;
+    description?: string;
+    url_title?: string;
+    url?: string;
+    children: JSX.Element | string;
+    bannerStyle?: CSSProperties;
+    bannerClassName?: string;
+    openInNewTab?: boolean; // New prop
 }
 
 function Section({
-  title,
-  description,
-  url,
-  url_title,
-  children,
-  bannerStyle,
-  bannerClassName,
-}: IProps) {
-  return (
-    <section className={section.container}>
-      <div className={section.blank} />
-      <Title level={1}>{title}</Title>
-      <p className={section.description}>
-        {description}{" "}
-        <a className={section.url} href={url}>
-          {url_title}
-        </a>
-      </p>
+                     title,
+                     description,
+                     url,
+                     url_title,
+                     children,
+                     bannerStyle,
+                     bannerClassName,
+                     openInNewTab = false, // Default value is false
+                 }: IProps) {
+    return (
+        <section className={section.container}>
+            <div className={section.blank}/>
+            <Title level={1}>{title}</Title>
+            <p className={section.description}>
+                {description}{" "}
+                <a
+                    className={section.url}
+                    href={url}
+                    target={openInNewTab ? "_blank" : "_self"} // Conditionally set target
+                    rel={openInNewTab ? "noopener noreferrer" : undefined} // Security measure for new tabs
+                >
+                    {url_title}
+                </a>
+            </p>
 
-      <div
-        className={`${section.banner} ${bannerClassName}`}
-        style={bannerStyle}
-      >
-        {children}
-      </div>
-    </section>
-  );
+            <div
+                className={`${section.banner} ${bannerClassName}`}
+                style={bannerStyle}
+            >
+                {children}
+            </div>
+        </section>
+    );
 }
 
 export default Section;
-
--- src/pages/Welcome/GuethubApp.tsx
+++ src/pages/Welcome/GuethubApp.tsx
@@ -1,36 +1,124 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Slider from "react-slick";
 import Section from "../../component/Section";
 import {useScrollHandler} from "../../hooks";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function GuethubApp() {
     const [ScrollAnimationRefs, addScrollAnimationRefs] = useScrollHandler();
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize(); // 初始检查窗口大小
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const mobileImages = [
+        "https://api.kexie.space/data/resource/hello/docs/introduction/appdev-assets/guethub-app-mobile-1.png",
+        "https://api.kexie.space/data/resource/hello/docs/introduction/appdev-assets/guethub-app-mobile-2.png",
+        "https://api.kexie.space/data/resource/hello/docs/introduction/appdev-assets/guethub-app-mobile-3.png",
+    ];
+
+
+    const settings = {
+        dots: true,
+        dotPosition: "bottom",
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        style: {
+            width: "100%",
+            height: "80%",
+            backgroundColor: "transparent",
+        }
+    };
 
     return (
         <Section
             title="GUET校园圈App"
             url="https://www.guethub.net/"
             url_title="点此前往官网下载"
+            openInNewTab={true}
             description="GUET校园圈 (GUET HUB)，支持 Android 和 IOS。
 
             它有几个非常实用的功能：
             1. 可以查看课表和成绩以及评教等教务功能，2.校内论坛功能。
 
             内测QQ群：482024244"
-            bannerStyle={{width: "75%", height: "80vh"}}
+            bannerStyle={{width: "80%", height: "auto", backgroundColor: "transparent"}}
         >
-            <img height="100%"
-                 width="100%"
-                 style={{
-                     boxShadow:
-                         "0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d",
-                     borderRadius: 16,
-                     paddingLeft: 150,
-                     paddingRight: 150,
-                     marginTop: 16
-                 }}
-                 ref={addScrollAnimationRefs}
-                 src="https://api.kexie.space/data/resource/hello/docs/introduction/appdev-assets/guethub-app.png"
-                 alt="屏幕截图"></img>
+            {isMobile ? (
+                <Slider
+                    {...settings}
+                >
+                    {mobileImages.map((image, index) =>
+                        <div key={index}>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    marginTop: "16px"
+                                }}
+                            >
+                                <img
+                                    src={image}
+                                    alt={`轮播图片 ${index + 1}`}
+                                    style={{
+                                        maxWidth: "100%",
+                                        maxHeight: "75vh",
+                                        objectFit: "cover",
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </Slider>
+            ) : (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80%", // 确保外部容器高度
+                        flexDirection: "row",
+                    }}
+                >
+                    {mobileImages.map((image, index) =>
+                        <div key={index}>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    marginTop: "16px",
+                                    marginLeft: "16px",
+                                    marginRight: "16px",
+                                }}
+                            >
+                                <img
+                                    src={image}
+                                    alt={`图片 ${index + 1}`}
+                                    style={{
+                                        maxWidth: "100%",
+                                        maxHeight: "75vh",
+                                        objectFit: "cover",
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
         </Section>
     );
 }
